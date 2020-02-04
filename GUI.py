f61ab160447ffe4657a1cae8b93069c468bb01ab
Your Name <sonykarki286@gmail.com>
2020-02-04 12:34:07 +0545

    added GUI

--- GUI.py
+++ GUI.py
@@ -1,9 +1,14 @@
-from guizero import App,Text
-from guizero import App,TextBox
-from guizero import App,PushButton
-app = App(title="Welome to reverse vending mahcine")
-welcome_message = Text(app,text="Please enter bottles in reversing vending machine")
-my_name = TextBox(app)
-update_text = PushButton(app,text="Display number of botles")
-app.display
+from guizero import App, Window, Text, PushButton
+def open_window():
+    window.show()
+def close_window():
+    window.hide()
+app = App(title="Welocme to reverse Vending Machine")
+text = Text(app,text="Please enter bottlein RVM")
+window = Window(app,title="welcome to reverse Vending Machine")
+text = Text(window,text="Thank you for entering bottles in RVM")
+window.hide()
+open_button = PushButton(app, text="Start", command=open_window)
+close_button = PushButton(window,text="End", command=close_window)
 
+app.display
